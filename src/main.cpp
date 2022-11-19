2bed34e631baeb08f8257fa5087f4ad0f37f9302
holombik-d <dmitriy.holombik@zagravagames.com>
2022-11-19 21:44:29 +0200

    init glad and test callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,25 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int _height = 460;
+int _width = 640;
+
+void WindowSizeCallback(GLFWwindow* window, int height, int width)
+{
+    _width = width;
+    _height = height;
+    glViewport(0, 0, _width, _height);
+}
+
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+        glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
 
 int main(void)
 {
@@ -9,21 +30,31 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(_width, _height, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
+    glfwSetWindowSizeCallback(window, WindowSizeCallback);
+    glfwSetKeyCallback(window, KeyCallback);
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if(gladLoadGL() == false)
+    {
+        return -1;
+    }
+
+
+    glClearColor(1, 0, 1, 0);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-       // glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
